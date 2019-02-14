391fb60129ddc27a97c7a89775bdfebb350d9cfc
Valerii Pravoslavnyi <valerio8787@gmail.com>
2019-02-14 11:34:37 +0200

    GLUE-3912: Added OauthCompanyUserInstallerPlugin to install company user scopes.

---
+++ src/Spryker/Zed/OauthCompanyUser/Communication/Plugin/Installer/OauthCompanyUserInstallerPlugin.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\OauthCompanyUser\Communication\Plugin\Installer;
+
+use Spryker\Zed\Installer\Dependency\Plugin\InstallerPluginInterface;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+/**
+ * @method \Spryker\Zed\OauthCompanyUser\Business\OauthCompanyUserFacadeInterface getFacade()
+ * @method \Spryker\Zed\OauthCompanyUser\OauthCompanyUserConfig getConfig()
+ */
+class OauthCompanyUserInstallerPlugin extends AbstractPlugin implements InstallerPluginInterface
+{
+    /**
+     * @api
+     *
+     * @return void
+     */
+    public function install()
+    {
+        $this->getFacade()->installCompanyUserOauthData();
+    }
+}
